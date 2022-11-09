58cc023d3a55724d92e1aa5c0701f72d77c7a746
Pragya Anand <anandpragya752@gmail.com>
2022-11-09 11:27:08 +0530

    assignmentjs2

---
+++ Assignment6.2/programs/pg1.js
@@ -0,0 +1,10 @@
+//1.Reverse an array? Input: [1, 2, 3, 4, 5, 6]
+
+const arrayReverse = (array)=>{
+array.reverse();
+return array;
+}
+
+const array1 = [1, 2, 3, 4, 5, 6]
+const reversedArray =  arrayReverse(array1);
+console.log(array1);
